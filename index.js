108568beb0fac192fbc04728379ea6128a78ec09
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-03-31 01:39:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    if (fun)
+    return `This Saturday, I want to ${fun}!`
+    else
+    return `This Saturday, I want to ${fun}!`
+}
+function mondayWork(activity="go to the office"){
+    if(activity)
+    return `This Monday, I will ${activity}.`
+    else
+    return `This Monday, I will ${activity}.`
+
+}
+function wrapAdjective(flair='*') {
+    return function(adjective='special'){
+    return `You are ${flair}${adjective}${flair}!`
+    };
+}
+
+
